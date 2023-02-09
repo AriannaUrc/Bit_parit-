cf4c886d90a85e40c53171836c498a1b51f688eb
URCIUOLI.22381 <URCIUOLI.22381@E2-9-PC16>
2023-02-09 08:55:48 +0100

    Aggiunta creazione di errori

--- Bit_parità/Program.cs
+++ Bit_parità/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleApp1
 {
     internal class Program
@@ -68,29 +69,43 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            int temp;
+
+            //chiedere una % di errori e genera una posizione random di tali errori
+
+            Random random= new Random();
+            int temp, lunghezza=7, percentErrori=30, x, y;
             string parola;
-            int[,] tabella = new int[7, 7];
-            int[] bit_paritaOr = new int[7];
-            int[] bit_paritaVert = new int[7];
+            int[,] tabella = new int[7, lunghezza];
+            int[,] tabellaMod = new int[7, lunghezza];
+
+            int[] PosizioniDis = new int[7 * lunghezza];
+            int max = 7 * lunghezza;
+
+            for (int i = 0; i < max; i++)
+            {
+                PosizioniDis[i] = i;
+            }
+
+            int[] bit_paritaOr = new int[lunghezza];
+            int[] bit_paritaVert = new int[lunghezza];
 
             Console.WriteLine("Inserire una parola di 7 caratteri: ");
 
             parola = Console.ReadLine();
 
-            while (parola.Length != 7)
+            while (parola.Length != lunghezza)
             {
                 Console.WriteLine("La parola inserita non è di una lunghezza adeguata\nInserire una nuova parola: ");
                 parola = Console.ReadLine();
             }
 
 
-
-            for (int i = 0; i < 7; i++)
+            //calcolo del valore in ASCII e binario e riempimento tabella
+            for (int i = 0; i < lunghezza; i++)
             {
                 temp = parola[i];
 
-                for (int j = 0; j < 7; j++)
+                for (int j = 0; j < lunghezza; j++)
                 {
                     tabella[i, j] = temp % 2;
                     temp = temp / 2;
@@ -100,18 +115,72 @@ namespace ConsoleApp1
             bit_paritaOr = parityOr(tabella);
             bit_paritaVert = parityVert(tabella);
 
-            for (int i = 0; i < 7; i++)
+
+            //scrittura tabella
+            for (int i = 0; i < lunghezza; i++)
             {
                 for (int j = 0; j < 7; j++)
                 {
                     Console.Write(tabella[i, j] + "\t");
                 }
+                //call e stampa bit di parità orizzontale
                 Console.Write(bit_paritaOr[i] + "\n");
             }
 
+           
+
+            //call e stampa bit di parità verticale
             for (int i = 0; i < 7; i++)
             {
                 Console.Write(bit_paritaVert[i]+"\t");
+            }
+
+            for (int i=0; i < lunghezza*7*percentErrori/100; i++) //lunghezza*7*percentErrori/100 = calcolo il numero di elementi nella tabella e trovo il numero corrispondente alla percentuale
+            {
+                //estraggo una delle posizioni disponibili
+                temp = random.Next(0, max);
+
+                x = PosizioniDis[temp] % 7;
+                y = PosizioniDis[temp] / 7;
+
+                if (tabellaMod[x, y] == 0)
+                {
+                    tabellaMod[x, y] = 1;
+                }
+                else
+                {
+                    tabellaMod[x, y] = 0;
+                }
+
+                //"cancello" dalle posizioni disponibili la posizione estratta
+                for (int j = temp; j < max - 1; j++)
+                {
+                    PosizioniDis[i] = PosizioniDis[i+1];
+                }
+
+                //diminuisco il numero di posizioni disponibili
+                max--;
+
+                
+            }
+
+            Console.WriteLine("\n\n\n");
+
+            //scrittura tabella
+            for (int i = 0; i < lunghezza; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Console.Write(tabellaMod[i, j] + "\t");
+                }
+                //call e stampa bit di parità orizzontale
+                Console.Write(bit_paritaOr[i] + "\n");
+            }
+
+            //call e stampa bit di parità verticale
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write(bit_paritaVert[i] + "\t");
             }
         }
     }
